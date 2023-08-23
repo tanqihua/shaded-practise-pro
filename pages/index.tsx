5d57652fa081952b70de9b6766447306ed2a13ac
掉链子了咯 <114892503+qihuatect@users.noreply.github.com>
2023-08-23 11:01:50 +0800

    pushh

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const showcase = ["day1", "day2", "day3"];
+  const router = useRouter();
   return (
-    <div>
-      <iframe src="/day2" />
-      <iframe src="/day1" />
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+      }}
+    >
+      {showcase.map((day, index) => {
+        return (
+          <div
+            key={index}
+            onClick={() => {
+              router.push(`/${day}`);
+            }}
+          >
+            <iframe
+              src={day}
+              key={index}
+              style={{
+                pointerEvents: "none",
+              }}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
